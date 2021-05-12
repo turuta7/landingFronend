c7494a8862f8259f1e5caec087d6aa600e5f9eb8
turuta7 <turuta51@gmail.com>
2021-05-12 07:49:15 +0300

    add put

--- src/text/AddCategory.js
+++ src/text/AddCategory.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import axios from "axios";
 import fs from "fs";
+import { title } from "process";
 
 const URLBackend = "http://localhost:4000";
 
@@ -9,35 +10,62 @@ const URL = `${URLBackend}/category`;
 console.log(URL);
 
 function AddCategory() {
-  const [text, setTextRu] = useState("");
-  const [textEn, setTextEn] = useState("");
-  const [avatar, setTextUa] = useState([]);
+  const [imgs, setImg] = useState();
 
+  useEffect(async () => {
+    try {
+      const data = await axios.get(URL);
+      console.log(data.data.img, link);
+      const image = data.data.img;
+      const aaa = new Buffer(image).toString("base64");
+
+      setImg(aaa);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const [link, setTextRu] = useState("");
+  const [textEn, setTextEn] = useState("");
+  const [avatar, setAvatar] = useState([]);
+
+  // const [picture, setPicture] = useState(null);
+  const formData = new FormData();
+
+  // const test = (e) => {
+  //   console.log("dd");
+  // };
+  // const config = { headers: {'Content-Type': 'multipart/form-data' } };
+  
+
+  
   const handleSubmit = async (evt) => {
     evt.preventDefault();
-    const formData = new FormData(); 
-    console.log("avatar",avatar);
+    
+    console.log("files", avatar);
+    console.log('link', link);
+    formData.append("link", link);
+    formData.append("3333", 4343);
+    formData.append("avatar", avatar[0]);
+    await axios ({
+      method: 'patch',
+      url: `${URL}/609aafb4abb5511fb8b04b09`,
+      data: formData
+      ,
+      headers: {'Content-Type': 'multipart/form-data' }
+      })
 
-    formData.append('avatar', avatar)
-    await axios(
-      {
-        method: "post",
-        url: `${URL}`,
-        headers: { "Content-Type": "multipart/form-data" },
+   
+    // axios.post(URL, formData);
+    setAvatar([]);
 
-        data:formData,
-      },
-
-      {
-        text,
-        avatar,
-      }
-    );
-    window.location.reload();
+    // window.location.reload();
   };
 
   return (
     <div className="center heading-background">
+      <img src={`data:image/png;base64,${imgs}`} />
+
       <p>
         <b>Add new category</b>
       </p>
@@ -46,8 +74,8 @@ function AddCategory() {
         <div>
           <span>textRu: </span>
           <textarea
-            type="text"
-            value={text}
+            type="link"
+            value={link}
             onChange={(e) => setTextRu(e.target.value)}
           />
           {/* <span>textEn: </span>
@@ -59,9 +87,10 @@ function AddCategory() {
           <span>file: </span>
           <input
             type="file"
-            value={avatar}
-            name="avatar"
-            onChange={(e) => setTextUa(e.target.value)}
+            onChange={(e) => setAvatar(e.target.files)}
+            // value={avatar1}
+            // onChange={test}
+            // name="avatar"
           />
         </div>
 
